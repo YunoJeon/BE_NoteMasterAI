a20f38daecd67a0cf4975ae10d6878e331bbd8f4
jeon-yuno <zero0577@naver.com>
2025-03-01 16:41:44 +0900

    프로젝트 생성

---
+++ src/main/java/com/be_notemasterai/BeNoteMasterAiApplication.java
@@ -0,0 +1,13 @@
+package com.be_notemasterai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeNoteMasterAiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BeNoteMasterAiApplication.class, args);
+  }
+
+}
